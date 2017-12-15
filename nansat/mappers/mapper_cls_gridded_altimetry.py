ea7c4b4e22452a03823a9e6dde0c78ba7f07ab16
akorosov <korosov@gmail.com>
2017-12-15 14:56:43 +0100

    #201 add switch cls mapper

--- nansat/mappers/mapper_cls_gridded_altimetry.py
+++ nansat/mappers/mapper_cls_gridded_altimetry.py
@@ -1,5 +1,13 @@
+import os
+from nansat.tools import WrongMapperError
 from nansat.mappers.mapper_netcdf_cf import Mapper as MapperNetcdfCF
 
 class Mapper(MapperNetcdfCF):
     def __init__(self, filename, gdal_dataset, gdal_metadata, *args, **kwargs):
-        pass
+        filebasename = os.path.basename(filename)
+        if not (filebasename.startswith('dt_global_allsat_phy_l4_') and
+                filebasename.endswith('nc')):
+            raise WrongMapperError
+        
+        super(Mapper, self).__init__(filename, gdal_dataset, gdal_metadata, *args, **kwargs)
+
